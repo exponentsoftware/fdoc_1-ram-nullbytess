2382bc5346ba4eb5d4bffb17b7811f25cb099cc5
ram-nullbytess <ram.mee1010@gmail.com>
2021-03-24 12:18:04 +0530

    day1 assignmnet

---
+++ 1/day1.1c.js
@@ -0,0 +1,10 @@
+function varietyOfWords(str) {
+    str = str.replace("/\S/", "");
+    str = str.replace(/(^\s*)|(\s*$)/gi,"");
+    str = str.replace(/[ ]{2,}/gi," ");
+    str = str.replace(/\n /,"\n");
+    
+    return str.split(' ').length;
+ }
+var sentence = "this is a sentence of having words to count"
+ console.log(varietyOfWords(sentence));
---
+++ 2/day1.2d.js
@@ -0,0 +1,14 @@
+function checkUniqueness(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {       
+        if (i !== j && arr[i] === arr[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  const arrOne = [1, 4, 6, 2, 1];
+  console.log(checkUniqueness(arrOne));
+  const arrTwo = [1, 4, 6, 2, 3]
+  console.log(checkUniqueness(arrTwo));
